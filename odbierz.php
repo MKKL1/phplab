6358a031457714855dbeb4af3fea864a5d52409a
MKKL1 <Mkkl3301@gmail.com>
2023-11-07 19:57:42 +0100

    Zadania po laboratorium 3 część 2

--- odbierz.php
+++ odbierz.php
@@ -12,47 +12,11 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         <div>
         <h2>Dane odebrane z formularza:</h2>
         <?php
-        //1
-//        if (isset($_REQUEST['login'])&&($_REQUEST['login']!="")) {
-//        $login = htmlspecialchars(trim($_REQUEST['login']));
-//        echo "Login: $login <br />";
-//        }
-//        else echo "Nie wpisano loginu <br />";
-
-        //2
-//        foreach ($_REQUEST as $key=>$value) {
-//            if(is_array($value)) {
-//                print("$key = [<br/>");
-//                foreach ($_REQUEST[$key] as $key2=>$value2) {
-//                    print("*$key2 = $value2 <br/>");
-//                }
-//                print("]<br/>");
-//            } else
-//            print("$key = $value <br/>");
-//        }
-        print("Wybrano tutoriale: " . join(" ",$_REQUEST['jezyki']) . "<br/>");
-        print("Sposob zaplaty: " . $_REQUEST['zaplata'] . "<br/>");
-//        if (isset($_REQUEST['nazwisko'])&&($_REQUEST['nazwisko']!="") &&
-//            isset($_REQUEST['wiek'])&&($_REQUEST['wiek']!="") &&
-//            isset($_REQUEST['email'])&&($_REQUEST['email']!="") &&
-//            isset($_REQUEST['panstwo'])&&($_REQUEST['panstwo']!=""))
-//
-        $doSprawdzenia = ['nazwisko','wiek','email','panstwo'];
-        $poprawne = true;
-        foreach ($doSprawdzenia as $value) {
-            if(!isset($_REQUEST[$value]) || $_REQUEST[$value]=="") {
-                $poprawne = false;
-                echo "$value nie ustawione!</br>";
-            }
+        if (isset($_REQUEST['login'])&&($_REQUEST['login']!="")) {
+        $login = htmlspecialchars(trim($_REQUEST['login']));
+        echo "Login: $login <br />";
         }
-        if($poprawne) {
-            $nazwisko = $_REQUEST['nazwisko'];
-            $wiek = $_REQUEST['wiek'];
-            $email = $_REQUEST['email'];
-            $panstwo = $_REQUEST['panstwo'];
-
-            print("<br/><a href='klient.php?nazwisko=$nazwisko&wiek=$wiek&email=$email&panstwo=$panstwo'>Dane klienta</a>");
-        }
+        else echo "Nie wpisano loginu <br />";
         ?>
         </div>
     </body>
--- pliki.php
+++ pliki.php
@@ -7,12 +7,13 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <div id="content">
     <main>
     <div>
-        <form action="odbierz.php" method="post">
+        <form action="pliki.php" method="post">
             <fieldset style="width:80%">
                 <legend>Formularz</legend>
                 <table>
@@ -54,15 +55,94 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
                 <input type="radio" name="zaplata" value="visa" /> visa <br />
                 <input type="radio" name="zaplata" value="przelew" /> przelew bankowy<br />
                 <br />
-<!--                <input type="reset" value="Wyczyść" />-->
-<!--                <input type="submit" name="zapisz" value="Zapisz" />-->
-<!--                <input type="submit" name="" value="Pokaż" />-->
-<!--                <input type="submit" name="submit" value="PHP" />-->
-<!--                <input type="submit" name="submit" value="CPP" />-->
-<!--                <input type="submit" name="submit" value="Java" />-->
+                <input type="reset" value="Wyczyść" />
+                <input type="submit" name="submit" value="Zapisz" />
+                <input type="submit" name="submit" value="Pokaż" />
+                <input type="submit" name="submit" value="PHP" />
+                <input type="submit" name="submit" value="CPP" />
+                <input type="submit" name="submit" value="Java" />
             </fieldset>
         </form>
     </div>
+        <?php
+        function dodaj() {
+            $dane = "";
+            dodaj_jeden($dane, "nazwisko", ";");
+            dodaj_jeden($dane, "wiek", ";");
+            dodaj_jeden($dane, "email", ";");
+            dodaj_jeden($dane, "panstwo", ";");
+            $dane .= htmlspecialchars(join(",",$_REQUEST["jezyki"])) . ";";
+            dodaj_jeden($dane, "zaplata", "");
+            $wp=fopen("plik.csv", "a",1);
+            fwrite($wp, $dane."\n");
+            fclose($wp);
+        }
+
+        function dodaj_jeden(&$dane, $nazwa, $end) {
+            if(isset($_REQUEST[$nazwa])) {
+                $dane .= htmlspecialchars($_REQUEST[$nazwa]).$end;
+            }
+        }
+
+        function pokaz($tut=null) {
+            $jezyki = ["C","CPP","Java","C#","HTML","CSS","XML","PHP","Javascript"];
+            print('<table class="tab"><thead>
+<tr>
+    <th rowspan="2">Nazwisko</th>
+    <th rowspan="2">Wiek</th>
+    <th rowspan="2">Mail</th>
+    <th rowspan="2">Panstwo</th>
+    <th rowspan="1">Jezyki</th>
+</tr>
+<tr>');
+
+            foreach ($jezyki as $jezyk) {
+                print("<th>$jezyk</th>");
+            }
+            print("</tr></thead><tbody>");
+            $wp=fopen("plik.csv", "r",1);
+            while(($dataline = fgetcsv($wp, 100, ';')) !== FALSE) {
+                print("<tr><td>$dataline[0]</td>");
+                print("<td>$dataline[1]</td>");
+                print("<td>$dataline[2]</td>");
+                print("<td>$dataline[3]</td>");
+                print(join(",", znajdzJezyki($jezyki, $dataline[4])));
+                foreach (znajdzJezyki($jezyki, $dataline[4]) as $value) {
+                    print($value);
+                }
+                print("</tr>");
+
+            }
+            print("</tbody></table>");
+            fclose($wp);
+        }
+
+        function znajdzJezyki($jezyki, $wczytane) {
+            $znalezione = [];
+            $i = 0;
+            foreach ($jezyki as $jezyk) {
+                $found = false;
+                foreach ($wczytane as $value) {
+                    if($jezyk===$value) {
+                        $found = true;
+                        break;
+                    }
+                }
+                if($found) $znalezione[$i] = true;
+                else $znalezione[$i] = false;
+                $i++;
+            }
+            return $znalezione;
+        }
+
+        if(isset($_REQUEST["submit"])) {
+            $akcja = $_REQUEST["submit"];
+            switch ($akcja) {
+                case "Zapisz": dodaj();break;
+                case "Pokaż": pokaz();break;
+            }
+        }
+        ?>
     </main>
 </div>
 
