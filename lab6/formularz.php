663140d6da1a914e2a9ad2b68500e8f8331f4479
Krystian Zielonka <s97779@pollub.edu.pl>
2023-11-23 17:38:48 +0100

    Lab 6

---
+++ lab6/formularz.php
@@ -0,0 +1,47 @@
+
+<form action="index.php" method="post">
+    <fieldset style="width:80%">
+        <legend>Formularz</legend>
+        <table>
+            <tr>
+                <td>Podaj nazwisko:</td>
+                <td><input type="text" name="nazwisko" pattern="[a-zA-Z0-9_-]{3,15}"/></td>
+            </tr>
+            <tr>
+                <td>Wiek:</td>
+                <td><input type="number" name="wiek"></td>
+            </tr>
+            <tr>
+                <td>Podaj email: </td>
+                <td><input type="email" name="email" pattern="[a-zA-Z0-9_]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+"/></td>
+            </tr>
+            <tr>
+                <td>Państwo: </td>
+                <td><select id="panstwo" name="panstwo">
+                        <option>Polska</option>
+                        <option>Inny</option>
+                    </select></td>
+            </tr>
+        </table>
+        <br /><br />
+        Zamawiam tutorial z języka: <br />
+        <?php
+        $jezyki = ["C","CPP","Java","C#","HTML","CSS","XML","PHP","Javascript"];
+        foreach($jezyki as &$v) {
+            print('
+        <input type="checkbox" id="lang'. $v . '" name="jezyki[]" value="'. $v . '" />
+        <label for="lang'. $v . '">'. $v . '</label>');
+
+        }
+        ?>
+        <br />
+
+        <br />Sposób zapłaty <br />
+        <input type="radio" name="zaplata" value="eurocard" /> eurocard <br />
+        <input type="radio" name="zaplata" value="visa" /> visa <br />
+        <input type="radio" name="zaplata" value="przelew" /> przelew bankowy<br />
+        <br />
+        <input type="submit" value="Wyślij" />
+        <input type="reset" value="Wyczyść" />
+    </fieldset>
+</form>
---
+++ lab6/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Lab5</title>
+</head>
+<body>
+<?php
+
+include "formularz.php";
+include_once "Baza.php";
+
+$bd = new Baza("localhost", "root", "", "klienci");
+function dodajdoBD(Baza $bd)
+{
+    $args = [
+        'nazwisko' => ['filter' => FILTER_VALIDATE_REGEXP, 'options' => ['regexp' => '/^[A-Z]{1}[a-ząęłńśćźżó-]{1,25}$/']],
+        'wiek' => ['filter' => [FILTER_VALIDATE_INT, FILTER_SANITIZE_FULL_SPECIAL_CHARS]],
+        'email' => ['filter' => [FILTER_VALIDATE_EMAIL, FILTER_SANITIZE_FULL_SPECIAL_CHARS]],
+        'panstwo' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+        'jezyki' => ['filter' => FILTER_SANITIZE_FULL_SPECIAL_CHARS, 'flags' => FILTER_REQUIRE_ARRAY],
+        'zaplata' => ['filter' => FILTER_SANITIZE_SPECIAL_CHARS]
+    ];
+    $dane = filter_input_array(INPUT_POST, $args);
+    //var_dump($dane);
+    $errors = "";
+    foreach ($dane as $key => $val) {
+        if ($val === false or $val === NULL) {
+            $errors .= $key . " ";
+        }
+    }
+    if ($errors === "") {
+        $nazwisko = $dane['nazwisko'];
+        $wiek = $dane['wiek'];
+        $email = $dane['email'];
+        $panstwo = $dane['panstwo'];
+        $jezyki = implode(",", $dane['jezyki']);
+        $zaplata = $dane['zaplata'];
+        $sql = "INSERT INTO klienci VALUES (NULL, '$nazwisko', '$wiek', '$panstwo', '$email', '$jezyki', '$zaplata');";
+        $bd->query($sql);
+    } else {
+
+    }
+
+}
+
+if (filter_input(INPUT_POST, "submit")) {
+    $akcja = filter_input(INPUT_POST, "submit");
+    switch ($akcja) {
+        case "Dodaj" :
+            dodajdoBD($bd);
+            break;
+        case "Pokaż" :
+            echo $bd->select("select Nazwisko, Zamowienie from klienci", ["Nazwisko", "Zamowienie"]);
+            break;
+    }
+}
+?>
+</body>
+</html>
